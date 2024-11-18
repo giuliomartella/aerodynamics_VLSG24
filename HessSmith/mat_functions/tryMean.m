6cb81dd59085cba2e8188b732efb646167a8baaf
giuliomartella <giulio.martella03@gmail.com>
2024-11-18 16:53:43 +0100

    fix aTh

--- HessSmith/mat_functions/tryMean.m
+++ HessSmith/mat_functions/tryMean.m
@@ -13,21 +13,21 @@ plot(airfoil.x, airfoil.y)
 hold on
 axis equal
 grid on
-plot(meanLine(:,1), meanLine(:,2))
+plot(meanLine.xMl, meanLine.yMl)
+plot(meanLine.xMl, meanLine.dy)
 
 
-h = 1e-10; % PROBLEM!! the integral result depens only on this element
-dy = meanLine(:,3);
-t = linspace(-0.5 +h, 0.5 -h, numel(dy))';
-aTh = trapz(t, dy./sqrt(0.25 - t.^2)) / pi
+
+t = linspace(-0.5, 0.5, numel(meanLine.dy))';
+dyf = @(x) spline(t, meanLine.dy, x);
+
+f_th = @(t) dyf(t)./sqrt(0.25 - t.^2);  
+
+aTh = 1/pi *integral(f_th,-0.5 ,0.5);  % using integral results are kinda better
 aTh = rad2deg(aTh)
 
+f_a0 = @(t) dyf(t).* sqrt((0.5 + t) / (0.5 -t));  
+a0 = 2/pi *integral(f_a0,-0.5 ,0.5);
+a0 = rad2deg(a0)
 
 
-dy = meanLine(:,3);
-t = linspace(-0.5, 0.5, numel(dy))';
-dyf = @(x) interp1(t, dy, x);
-f = @(t) dyf(t)./sqrt(0.25 - t.^2);  
-
-aTh = 1/pi *integral(f,-0.5 ,0.5)  % using integral results are kinda better
-aTh = rad2deg(aTh)
