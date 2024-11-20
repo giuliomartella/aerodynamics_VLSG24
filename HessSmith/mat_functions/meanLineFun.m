75d2d3351a705e66ad84b4b1489a746398ccd731
giuliomartella <giulio.martella03@gmail.com>
2024-11-20 18:36:36 +0100

    add rotation

--- HessSmith/mat_functions/meanLineFun.m
+++ HessSmith/mat_functions/meanLineFun.m
@@ -1,10 +1,18 @@
 function meanLine = meanLineFun(airfoil)
 % MEANLINEFUN computes the mean camber line of an airfoil using spline coefficients.
 
+% correct trailing edge
+meanLine.tau = atan2(airfoil.y(1), 1);
+rotation = [cos(meanLine.tau), -sin(meanLine.tau); sin(meanLine.tau), cos(meanLine.tau)];
+for i = 1:numel(airfoil.x)
+    rotated_X = rotation \ [airfoil.x(i); airfoil.y(i)];
+    airfoil.x(i) = rotated_X(1);  airfoil.y(i) = rotated_X(2); 
+end
+
 lower = false;
 xl = []; yl = []; xu = []; yu = []; 
 for i = 1:numel(airfoil.x)
-    lower = lower || (airfoil.y(i) < 1e-10);
+    lower = lower || (airfoil.y(i) < 0);
     if lower
         xl = [xl; airfoil.x(i)];
         yl = [yl; airfoil.y(i)];
@@ -29,6 +37,7 @@ dy = [dy(1) dy];
 cfit=fit(meanLine.xMl(:),dy(:),'smoothingspline');
 meanLine.dy = feval(cfit, meanLine.xMl)';
 
-% meanLine.dy = dy;
+meanLine.x = airfoil.x;
+meanLine.y = airfoil.y;
 
  end
--- HessSmith/mat_functions/tryMean.m
+++ HessSmith/mat_functions/tryMean.m
@@ -3,13 +3,13 @@ clc
 close all
 
 addpath airfoils
-filename = 'k2_1.dat';
+filename = 'k2.dat';
 airfoil = importXfoilProfile(filename);
 
 meanLine = meanLineFun(airfoil);
 
 figure
-plot(airfoil.x, airfoil.y)
+plot(meanLine.x, meanLine.y)
 hold on
 axis equal
 grid on
@@ -17,17 +17,16 @@ plot(meanLine.xMl, meanLine.yMl)
 plot(meanLine.xMl, meanLine.dy)
 
 
-
-t = linspace(-0.5, 0.5, numel(meanLine.dy))';
-dyf = @(x) spline(t, meanLine.dy, x);
+h = 1e-6;
+t = linspace(-0.5+h, 0.5-h, numel(meanLine.dy))';
+dyf = @(x) interp1(t, meanLine.dy, x);
 
 f_th = @(t) dyf(t)./sqrt(0.25 - t.^2);  
-
-aTh = 1/pi *integral(f_th,-0.5 ,0.5);  % using integral results are kinda better
+aTh = 1/pi *integral(f_th,t(1), t(end));  % using integral results are kinda better
 aTh = rad2deg(aTh)
 
 f_a0 = @(t) dyf(t).* sqrt((0.5 + t) / (0.5 -t));  
-a0 = 2/pi *integral(f_a0,-0.5 ,0.5);
+a0 = 2/pi *integral(f_a0,t(1), t(end));
 a0 = rad2deg(a0)
 
 
